e94473b294e42996f7cccd131a9db51bd7e86820
Mike Apicella <mike.g.apicella@gmail.com>
2018-06-12 23:55:41 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+    <footer>
+        <p>This is my footer</p>
+        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+    </footer>
+
+    <?php 
+
+        
+        wp_footer(); 
+
+    ?>
+
+
+</body>
+</html>
